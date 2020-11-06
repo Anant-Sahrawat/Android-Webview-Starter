17c4efd5b07c6a6946e155dbc94c898ce752a5e5
Anant Sahrawat <anant.s@gigindia.in>
2020-11-06 19:19:28 +0530

    Added Starter Webview

--- app/src/main/java/me/anants/webviewstarter/MainActivity.java
+++ app/src/main/java/me/anants/webviewstarter/MainActivity.java
@@ -1,14 +1,354 @@
 package me.anants.webviewstarter;
 
-import androidx.appcompat.app.AppCompatActivity;
+/*
+Starter Repo - https://github.com/Anant-Sahrawat/Android-Webview-Starter
+ */
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.GeolocationPermissions;
+import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String url = "https://google.com";
+
+    private WebView mWebView;
+    private ImageView splash;
+    private ImageView noInternet;
+    private int state = 0;
+    private ValueCallback<Uri> mUploadMessage;
+    public ValueCallback<Uri[]> uploadMessage;
+    private String mCameraPhotoPath;
+    private final static int FILECHOOSER_RESULTCODE = 1;
+    private final static int INPUT_FILE_REQUEST_CODE = 2;
+
+    private final static int CAMERA_PERMISSIONS = 10;
+    private final static int LOCATION_PERMISSIONS = 11;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = findViewById(R.id.webview);
+        splash = findViewById(R.id.splash);
+        noInternet = findViewById(R.id.internet_error);
+
+        noInternet.setVisibility(View.GONE);
+        mWebView.setVisibility(View.GONE);
+        splash.setVisibility(View.VISIBLE);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                mWebView.setVisibility(View.VISIBLE);
+                splash.setVisibility(View.GONE);
+                if (state == 1) {
+                    noInternet.setVisibility(View.VISIBLE);
+                    mWebView.setVisibility(View.GONE);
+                }
+
+            }
+
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                super.shouldOverrideUrlLoading(view, request);
+
+                if (request.getUrl().toString().startsWith("tel:")) {
+                    startActivity(new Intent(Intent.ACTION_DIAL, request.getUrl()));
+                    return true;
+                }
+
+                if (request.getUrl().toString().startsWith(url)) {
+                    if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                            || ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                            || ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                                Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                Manifest.permission.CAMERA
+                        }, LOCATION_PERMISSIONS);
+                    }
+                }
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (error.getErrorCode() == -2) {
+                        mWebView.stopLoading();
+                        state = 1;
+                    }
+                }
+            }
+        });
+
+        mWebView.setInitialScale(1);
+        mWebView.setBackgroundColor(Color.TRANSPARENT);
+        WebSettings mWebSettings = mWebView.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        mWebSettings.setAllowFileAccess(true);
+        mWebSettings.setAllowContentAccess(true);
+        mWebSettings.setAllowFileAccessFromFileURLs(true);
+        mWebSettings.setDomStorageEnabled(true);
+        mWebSettings.setGeolocationEnabled(true);
+        mWebSettings.setLoadWithOverviewMode(true);
+        mWebSettings.setUseWideViewPort(true);
+
+        mWebView.addJavascriptInterface(new JavaScriptShareInterface(), "Android");
+
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            CookieManager.getInstance().setAcceptThirdPartyCookies(mWebView, true);
+        } else {
+            CookieManager.getInstance().setAcceptCookie(true);
+        }
+
+        mWebView.loadUrl(url);
+
+        mWebView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                locationGpsCheck();
+                callback.invoke(origin, true, false);
+            }
+
+
+            // For Lollipop 5.0+ Devices
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
+                if (uploadMessage != null) {
+                    uploadMessage.onReceiveValue(null);
+                    uploadMessage = null;
+                }
+
+                uploadMessage = filePathCallback;
+
+                //Check whether Camera and Write Permissions are Given
+                if ((ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+                        && (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)) {
+
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                        // Create the File where the photo should go
+                        File photoFile = null;
+                        try {
+                            photoFile = createImageFile();
+                            takePictureIntent.putExtra("PhotoPath", mCameraPhotoPath);
+                        } catch (IOException ex) {
+                            // Error occurred while creating the File
+                            Log.e("ErrorCreatingFile", "Unable to create Image File", ex);
+                        }
+
+                        // Continue only if the File was successfully created
+                        if (photoFile != null) {
+                            mCameraPhotoPath = "file:" + photoFile.getAbsolutePath();
+                            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                                    FileProvider.getUriForFile(getApplicationContext(), BuildConfig.APPLICATION_ID + ".fileprovider", photoFile));
+                        } else {
+                            takePictureIntent = null;
+                        }
+                    }
+
+                    Intent fileChoosingIntent = fileChooserParams.createIntent();
+
+                    Intent[] intentArray;
+                    if (takePictureIntent != null) {
+                        intentArray = new Intent[]{takePictureIntent};
+                    } else {
+                        intentArray = new Intent[0];
+                    }
+
+                    Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                    chooserIntent.putExtra(Intent.EXTRA_INTENT, fileChoosingIntent);
+                    chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser");
+                    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+
+                    startActivityForResult(chooserIntent, INPUT_FILE_REQUEST_CODE);
+                } else {
+                    // If permissions not given ask for these permissions
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.CAMERA
+                    }, CAMERA_PERMISSIONS);
+
+                }
+
+                return true;
+            }
+        });
+
     }
+
+    private void locationGpsCheck() {
+        LocationManager lm = (LocationManager) getApplicationContext().getSystemService(getApplicationContext().LOCATION_SERVICE);
+        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageGps();
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + ".jpg";
+        File imageFile = new File(this.getFilesDir(), imageFileName);
+        return imageFile;
+    }
+
+    private void buildAlertMessageGps() {
+        AlertDialog builder = new AlertDialog.Builder(this).create();
+        builder.setTitle("Turn On Location");
+        builder.setMessage("We require location only in cases where it is required to get entry location.");
+        builder.setCancelable(false);
+        builder.setButton("Turn On Location", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        });
+        builder.show();
+    }
+
+    public class JavaScriptShareInterface {
+
+        @JavascriptInterface
+        public void share(final String message) {
+            Intent myShareIntent = new Intent();
+            myShareIntent.setAction(Intent.ACTION_SEND);
+            myShareIntent.putExtra(Intent.EXTRA_TEXT, message);
+            myShareIntent.setType("text/plain");
+            startActivity(Intent.createChooser(myShareIntent, "Share Using"));
+        }
+
+        @JavascriptInterface
+        public void shortToast(final String message) {
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        }
+
+        @JavascriptInterface
+        public void longToast(final String message) {
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+        }
+
+        @JavascriptInterface
+        public String appVersion(){
+            return String.valueOf(BuildConfig.VERSION_CODE);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case LOCATION_PERMISSIONS: {
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // LOCATION PERMISSIONS GRANTED, CHECKING FOR LOCATION SERVICE
+                    locationGpsCheck();
+                } else {
+                    Toast.makeText(this, "Please provide these permissions so we can get Store Location", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            Uri[] results = null;
+
+            // Check that the response is a good one
+            if (resultCode == Activity.RESULT_OK) {
+                if (intent == null) {
+                    // If there is not data, then we may have taken a photo
+                    if (mCameraPhotoPath != null) {
+                        results = new Uri[]{Uri.parse(mCameraPhotoPath)};
+                    }
+                } else {
+                    String dataString = intent.getDataString();
+                    if (dataString != null) {
+                        results = new Uri[]{Uri.parse(dataString)};
+                    }
+                }
+            }
+
+            uploadMessage.onReceiveValue(results);
+            uploadMessage = null;
+        } else if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (null == mUploadMessage)
+                return;
+            // Use MainActivity.RESULT_OK if you're implementing WebView inside Fragment
+            // Use RESULT_OK only if you're implementing WebView inside an Activity
+            Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        } else
+            Toast.makeText(getApplicationContext(), "Failed to Upload Image", Toast.LENGTH_LONG).show();
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public void onBackPressed() {
+        String current = mWebView.getUrl();
+
+        if (current.startsWith(url)) {
+            finishAffinity();
+            finish();
+        }
+
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
 }
